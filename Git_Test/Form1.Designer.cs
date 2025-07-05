a8a1656d5360f566cb4d3e2691de89e8080058aa
VBAke <massage19830907@yahoo.co.jp>
2025-07-05 13:03:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Git_Test/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Git_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtChangeContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "リポジトリ作成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "コミット";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtChangeContent
+            // 
+            this.txtChangeContent.Location = new System.Drawing.Point(184, 107);
+            this.txtChangeContent.Name = "txtChangeContent";
+            this.txtChangeContent.Size = new System.Drawing.Size(303, 19);
+            this.txtChangeContent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "変更内容について";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtChangeContent);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtChangeContent;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Git_Test/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+
+
+namespace Git_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newRepoPath = Repository.Init(@"C:\Users\massa\Documents\Test");
+
+            MessageBox.Show(newRepoPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string gitPath = @"C:\Users\massa\Documents\Test";
+            string strComment = txtChangeContent.Text;
+            if (strComment.Length < 3)
+            {
+                MessageBox.Show("変更内容のコメントは３文字以上必要です");
+                return;
+            }
+
+            //コミットを実施
+            using (var repo = new Repository(gitPath))
+            {
+                //テキストファイルを作成(トライ敵に実施するため)
+                //var content = "Commit this!";
+                //File.WriteAllText(Path.Combine(repo.Info.WorkingDirectory, gitPath + @"\fileToCommit.txt"), content);
+
+                // ファイルをステージングする(対象ファイルを選ぶ)
+                repo.Index.Add("fileToCommit.txt");
+                repo.Index.Add("Git_test.txt");
+                repo.Index.Write();
+
+                // コミット情報を作成
+                Signature author = new Signature("sakai", "@jugglingnutcase", DateTime.Now);
+                Signature committer = author;
+
+                // リポジトリにコミットを実施
+                Commit commit = repo.Commit(strComment, author, committer);
+            }
+        }
+    }
+}
